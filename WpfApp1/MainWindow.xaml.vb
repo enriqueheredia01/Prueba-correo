acde1c533c93fb15eb5f91fcedf51d7e92ed06da
Enrique Heredia <enriqueheredia.e@outlook.com>
2019-10-09 10:37:29 -0500

    Agregar archivos de proyecto.

---
+++ WpfApp1/MainWindow.xaml.vb
@@ -0,0 +1,20 @@
+﻿Imports System.Net.Mail
+
+Class MainWindow
+    Private Sub Button_Click(sender As Object, e As RoutedEventArgs) Handles button.Click
+        button.Visibility = Visibility.Hidden
+        Try
+            Dim SMTP As New SmtpClient
+            'Configuración SMTP
+            'SMTP.UseDefaultCredentials = False
+            SMTP.Credentials = New Net.NetworkCredential("info@sos-food.com", "sosfood2019**escritorioseguro")
+            SMTP.Port = 587
+            SMTP.EnableSsl = True
+            SMTP.Host = "mail.sos-food.com"
+            SMTP.Send("info@sos-food.com", "enriqueheredia.e@gmail.com", "Hola", "un saludo makina.......")
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+        button.Visibility = Visibility.Visible
+    End Sub
+End Class
---
+++ WpfApp3/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net.Mail;
+using System.Net;
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            SmtpClient SMTP = new SmtpClient();
+            SMTP.UseDefaultCredentials = false;
+
+            ICredentialsByHost cred;
+            
+            SMTP.Credentials = ICredentialsByHost //("info@sos-food.com", "sosfood2019**escritorioseguro");
+            SMTP.Port = 25
+            SMTP.EnableSsl = False
+            SMTP.Host = "mail.sos-food.com"
+            SMTP.Send("info@sos-food.com", "enriqueheredia.e@gmail.com", "Hola", "un saludo makina.")
+        }
+    }
+}
